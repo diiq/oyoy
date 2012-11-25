56ed0f0896049b2ca6d36ff9a564300c428441ab
Sam Bleckley <diiq.diiq@gmail.com>
2012-11-25 15:08:17 +1300

    Misunderstood rpython dicts

--- interpreter.py
+++ interpreter.py
@@ -7,37 +7,37 @@ An interpreter for a code-as-object version of Oyster, as opposed to code-as-lis
 current = ""
 
 def eval(code, env):
-    if code.type == "call":
+    if code["type"] == "call":
         
-        function = eval(code.call, env)
+        function = eval(code["call"], env)
 
-        new_env = dict(function.bindings)
-        for arg in function.args:
-            new_env[arg.name] = eval_arg(arg, code.args[arg.name], env)
+        new_env = dict(function["bindings"])
+        for arg in function["args"]:
+            new_env[arg["name"]] = eval_arg(arg, code["args"][arg["name"]], env)
 
         return apply_fn(function, new_env)
         
-    elif (code.type == "builtin" or
-          code.type == "number"):
+    elif (code["type"] == "builtin" or
+          code["type"] == "number"):
         return code
 
-    elif code.type == "symbol":
+    elif code["type"] == "symbol":
         return lookup(env, code)
 
 def eval_arg(arg, code, env):
-    if arg.code:
+    if arg["code"]:
         return arg 
     else:
         return eval(code, env)
 
 def lookup(env, symbol):
-    return env[symbol.value]
+    return env[symbol["id"]]
 
 def apply_fn(function, env):
-    if function.type == "builtin":
-        return function.function(env)
+    if function["type"] == "builtin":
+        return function["function"](env)
     else:
-        return eval(function.body, env)
+        return eval(function["body"], env)
 
 
 # That's all, folks!
@@ -46,40 +46,46 @@ def apply_fn(function, env):
 # function call, like you see below. Should perhaps be replaced by
 # separate classes, but those are so *verbose*...
 
-class Atom(object):
-    def __init__(self, typ, value):
-        self.type = typ
-        self.value = value
 
-class Arg(object):
-    def __init__(self, name):
-        self.type = "arg"
-        self.name = name
-        self.code = False
-
-class Call(object):
-    def __init__(self, call, kwargs):
-        self.type = "call"
-        self.call = call
-        self.args = kwargs
-
-class Lambda(object):
-    def __init__(self, args, body, env):
-        self.type = "lambda"
-        self.args = args
-        self.body = body
-        self.bindings = env
-
-class Builtin(object):
-    def __init__(self, function, args):
-        self.type = "builtin"
-        self.args = args
-        self.function = function
-        self.bindings = {}
-
+def Atom(typ, value):
+    self = {}
+    self["type"] = typ
+    self["value"] = value
+    return self
+    
+def Arg(name):
+    self = {}
+    self["type"] = "arg"
+    self["name"] = name
+    self["code"] = False
+    return self
+    
+def Call(call, kwargs):
+    self = {}
+    self["type"] = "call"
+    self["call"] = call
+    self["args"] = kwargs
+    return self
+    
+def Lambda(args, body, env):
+    self = {}
+    self["type"] = "lambda"
+    self["args"] = args
+    self["body"] = body
+    self["bindings"] = env
+    return self
+    
+def Builtin(function, args):
+    self = {}
+    self["type"] = "builtin"
+    self["args"] = args
+    self["function"] = function
+    self["bindings"] = {}
+    return self
+    
 def builtin_plus(env):
-    x = env["x"].value
-    y = env["y"].value
+    x = env["x"]["value"]
+    y = env["y"]["value"]
     return Atom("number", x+y)
     
 plus = Builtin(builtin_plus, [Arg("x"), Arg("y")])
@@ -92,7 +98,7 @@ def entry_point(argv):
                      "y" : Call(plus, 
                              {"x" : Atom("number", 3), 
                               "y" : Atom("number", 5)})})
-    print eval(testcode, {}).value
+    print eval(testcode, {})["value"]
     return 0
     
 def target(*args):
