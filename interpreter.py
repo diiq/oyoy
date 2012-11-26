0d776ce42f76b947638750123bf281962d996593
Sam Bleckley <diiq.diiq@gmail.com>
2012-11-26 13:24:38 +1300

    Ohgosh forgot the classes.

--- interpreter.py
+++ interpreter.py
@@ -2,8 +2,44 @@
 An interpreter for a code-as-object version of Oyster, as opposed to code-as-list.
 """
 
-from oy_objects import *
+class OyO(object):
+    pass
 
+class Number(OyO):
+    def __init__(self, value):
+        self.number = value
+
+class Symbol(OyO):
+    def __init__(self, value):
+        self.symbol = value
+
+class Arg(OyO):
+    def __init__(self, name, code=False):
+        self.name = name
+        self.code = code
+
+class Call(OyO):
+    def __init__(self, call, kwargs):
+        self.call = call
+        self.args = kwargs
+
+class Lambda(OyO):
+    def __init__(self, args, body, env):
+        self.lambda_list = args
+        self.body = body
+        self.bindings = env
+
+class Builtin(OyO):
+    def __init__(self, function, args, bindings={}):
+        self.function = function
+        self.lambda_list = args
+        self.bindings = bindings
+
+class List(OyO):
+    def __init__(self, items):
+        self.items = items
+
+        
 FAIL = Symbol("Fail")
 
 def eval(code, env):
