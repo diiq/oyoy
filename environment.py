8c2e6fb7facb8d1d4a31250b8c7563fec05fd0c6
Sam Bleckley <diiq.diiq@gmail.com>
2012-11-27 13:07:28 +1300

    Commentary on environments.

--- environment.py
+++ environment.py
@@ -1,3 +1,24 @@
+"""
+Environment class for Oyster. 
+
+Oyster has two ways to lookup symbol bindings, and three ways to bind
+symbols. 
+
+The default lookup looks in lexical scope. If a symbol has been
+"leaked" from the current scope, it is looked for in the dynamic scope.
+
+The default setting behavior is to bind symbols locally only.
+If a symbol has been leaked, it is set in dynamic scope.
+If a symbol has been altered, it is set in lexical scope.
+
+This behavior means no `let`, `let*` or `with` is necessary.
+
+It also means pure functions can be identified by simple, local static
+analysis.
+
+"""
+
+
 class Env():
     def __init__(self, lexical, dynamic):
         self.active = {} # active is where local variables are set
