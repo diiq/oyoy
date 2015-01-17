3026f2f9b9514f7689fbeeb15e56d91bfcc693ad
Sam Bleckley <sam@mutuallyhuman.com>
2015-01-17 14:43:30 -0500

    It's a lexer!

---
+++ parser/scanner.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+import plex
+
+from interpreter.code_objects import Symbol, Call, Number
+
+lmbda = plex.Str("λ")
+letter = plex.Range("AZaz") | lmbda
+digit = plex.Range("09")
+valid_punctuation = plex.Any("-+?!$~<>_")
+symbol = (letter + plex.Rep(letter | digit | valid_punctuation) |
+          valid_punctuation)
+number = plex.Rep1(digit)
+open = plex.Str("(")
+close = plex.Str(")")
+space = plex.Any(" \t")
+colon = plex.Str(":")
+indentation = plex.Rep(plex.Str(" "))
+lineterm = plex.Str("\n") | plex.Eof
+comment = plex.Str("#") + plex.Rep(plex.AnyBut("\n"))
+blank_line = indentation + plex.Opt(comment) + lineterm
+
+class OysterScanner(plex.Scanner):
+    def __init__(self, file, name):
+        # The indendation stack stores the current indendation depth
+        # in characters
+        self.indentation_stack = [0]
+
+        # The paren stack stores how far back into the indentation
+        # stack to pop when we close the next paren.
+        self.paren_stack = [0]
+
+        plex.Scanner.__init__(self, self.lexicon, file, name)
+
+    def current_level(self):
+        return self.indentation_stack[-1]
+
+    def open_action(self, text):
+        position = self.position()[2]
+        self.indentation_stack.append(position + 1)
+        self.paren_stack.append(len(self.indentation_stack))
+        return "open"
+
+    def close_action(self, text):
+        ind = self.paren_stack.pop()
+        while len(self.indentation_stack) > ind:
+            self.produce('dedent', '')
+            self.indentation_stack.pop()
+        self.indentation_stack.pop()
+
+        return "close"
+
+    def indentation_action(self, text):
+        current_level = self.current_level()
+        new_level = len(text)
+        if new_level > current_level:
+            self.indent_to(new_level)
+        elif new_level < current_level:
+            self.dedent_to(new_level)
+        else:
+            self.produce('nodent', '')
+        self.begin('')
+
+    def indent_to(self, new_level):
+        self.indentation_stack.append(new_level)
+        self.produce('indent', '')
+
+    def dedent_to(self, new_level):
+        while new_level < self.current_level():
+            self.indentation_stack.pop()
+            self.produce('dedent', '')
+
+    def eof(self):
+        self.dedent_to(0)
+
+    def lineterm_action(self, text):
+        self.begin('new_line')
+
+
+    lexicon = plex.Lexicon([
+        (symbol,        "Symbol"),
+        (number,        "Number"),
+        (open,          open_action),
+        (close,         close_action),
+        (colon,         "colon"),
+        (space,         plex.IGNORE),
+        (lineterm,      lineterm_action),
+        plex.State('new_line', [
+            (blank_line,    plex.IGNORE),
+            (indentation,   indentation_action),
+        ]),
+    ])
---
+++ tests/test_scanner.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+from cStringIO import StringIO
+from textwrap import dedent
+from pprint import pformat
+
+from test_case import TestCase
+from interpreter.code_objects import *
+from parser.scanner import OysterScanner
+
+
+class ScannerTests(TestCase):
+    def assertScansTo(self, scanner, expected):
+        res = (scanner.read()[0] for i in xrange(10000))
+        pairs = zip(expected, res)
+        if not all([pair[0] == pair[1] for pair in pairs]):
+            raise AssertionError("Scan failed to match:\n%s" % pformat(pairs, 4))
+
+    def test_symbol(self):
+        file = StringIO("a-symbol")
+        scanner = OysterScanner(file, "test_symbol")
+        res = scanner.read()
+        self.assertEqual(res[0], "Symbol")
+        self.assertEqual(res[1], "a-symbol")
+
+    def test_number(self):
+        file = StringIO("242")
+        scanner = OysterScanner(file, "test_number")
+        res = scanner.read()
+        self.assertEqual(res[0], "Number")
+        self.assertEqual(res[1], "242")
+
+    def test_chain(self):
+        file = StringIO("(a-symbol 3 b)")
+        scanner = OysterScanner(file, "test_close")
+        expected = ["open",
+                    "Symbol",
+                    "Number",
+                    "Symbol",
+                    "close"]
+
+        self.assertScansTo(scanner, expected)
+
+    def test_indentation(self):
+        code = """
+        this:
+            is
+        sparta
+        """
+        file = StringIO(dedent(code))
+        scanner = OysterScanner(file, "test_indentation")
+        expected = ["nodent", "Symbol", "colon",
+                    "indent", "Symbol",
+                    "dedent", "Symbol", "nodent", None]
+
+        self.assertScansTo(scanner, expected)
+
+    def test_parens_and_indentation(self):
+        code = """
+        (this
+         is:
+             mine) 5
+        sparta
+        """
+        file = StringIO(dedent(code))
+        scanner = OysterScanner(file, "test_parens_and_indentation")
+        expected = ["nodent", "open", "Symbol",
+                    "nodent", "Symbol", "colon",
+                    "indent", "Symbol", "dedent", "close", "Number",
+                    "nodent", "Symbol", "nodent", None]
+
+        self.assertScansTo(scanner, expected)
+
+    def test_plus_program(self):
+        code = """
+        set my-plus: λ(x y):
+            # commentary goes here
+            + x y
+
+        my-plus 2 (+ 3 5)
+        """
+        file = StringIO(dedent(code))
+        scanner = OysterScanner(file, "test_plus")
+        expected =  ['nodent', 'Symbol', 'Symbol', 'colon',
+                       'Symbol', 'open', 'Symbol', 'Symbol', 'close', 'colon',
+                     'indent', 'Symbol', 'Symbol', 'Symbol',
+                     'dedent', 'Symbol', 'Number',
+                       'open', 'Symbol', 'Number', 'Number', 'close',
+                     'nodent', None]
+
+        self.assertScansTo(scanner, expected)
